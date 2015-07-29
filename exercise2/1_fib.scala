23434e7c5b6f4a5544f09fe64d921d74f000a086
ozawaseijiro <seijiro4751@gmail.com>
2015-07-30 07:36:05 +0900

    update test

--- exercise2/1_fib.scala
+++ exercise2/1_fib.scala
@@ -1,13 +1,17 @@
 
-def fib(n: Int) = {
-  @annotation.tailrec
-  def go(n: Int, prev: Int, current: Int): Int =
-    if(n == 0) prev
-    else go(n-1, current, prev + current)
+object Excercise1{
+  def fib(n: Int) = {
+    @annotation.tailrec
+    def go(n: Int, prev: Int, current: Int): Int =
+      if(n == 0) prev
+      else go(n-1, current, prev + current)
 
-  go(n, 0, 1)
-}
+      go(n, 0, 1)
+    }
 
-def main(args: Array[String]): Unit = {
-  println(fib(6))
-}
+    def main(args: Array[String]): Unit = {
+      if(fib(6) == 8) println("correct")
+      else println("incorrect")
+    }
+
+  }
--- exercise2/2_sorted.scala
+++ exercise2/2_sorted.scala
@@ -1,8 +1,24 @@
-def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-  def go(n: Int, prev:A): Boolean = {
-    if(as.length <= n) true
-    else if(ordered(prev, as(n))) go(n+1, as(n))
-    else false
+object HelloWorld{
+
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int, prev:A): Boolean = {
+      if(as.length <= n) true
+      else if(ordered(prev, as(n))) go(n+1, as(n))
+      else false
+    }
+    go(1, as(0))
   }
-  go(1, as(0))
+
+  def main(args: Array[String]): Unit = {
+    val a = isSorted[Int](Array(1,2,3,4,5, 1), (left :Int, right:Int) => left < right)
+    if(a) println("incorrect")
+
+    val b = isSorted[Int](Array(1,2,3,4,5), (left :Int, right:Int) => left < right)
+    if(!b) println("incorrect")
+
+    println("correct")
+  }
+
 }
